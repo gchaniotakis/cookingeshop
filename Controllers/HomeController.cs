b91518de7e200eefd41bddcd98cc84d7a40b5fa7
Georgios Chaniotakis <haniotinho@gmail.com>
2019-09-05 14:13:39 +0300

    home controller added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CookingEShop.Models;
+
+namespace CookingEShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pie)
+        {
+            _pieRepository = pie;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
