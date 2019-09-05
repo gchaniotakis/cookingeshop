d972364468a95074724bb1854419d4c884f84a25
Georgios Chaniotakis <haniotinho@gmail.com>
2019-09-05 14:04:49 +0300

    service addition

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using CookingEShop.Models;
 
 namespace CookingEShop
 {
@@ -15,6 +16,9 @@ namespace CookingEShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            
+
             services.AddMvc();
         }
 
