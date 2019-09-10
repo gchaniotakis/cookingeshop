96dfcf3a66ced316dbd4265ac7dd6fc283448df7
Georgios Chaniotakis <haniotinho@gmail.com>
2019-09-10 09:41:05 +0300

    model changed

--- Migrations/20190910064009_PieModelChanged.Designer.cs
+++ Migrations/20190910064009_PieModelChanged.Designer.cs
@@ -3,14 +3,16 @@ using CookingEShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CookingEShop.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190910064009_PieModelChanged")]
+    partial class PieModelChanged
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Models/Pie.cs
+++ Models/Pie.cs
@@ -22,5 +22,7 @@ namespace CookingEShop.Models
         public string ImageThumbnailUrl { get; set; }
 
         public bool IsPieOfTheWeek { get; set; }
+
+        public bool IsInStock { get; set; }
     }
 }
