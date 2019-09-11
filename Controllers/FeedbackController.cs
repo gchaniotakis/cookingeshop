803fcc801e97b1bc0ff1174c0845cfd9ad32832c
Georgios Chaniotakis <haniotinho@gmail.com>
2019-09-11 10:16:51 +0300

    form creation

---
+++ Controllers/FeedbackController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CookingEShop.Models;
+
+namespace CookingEShop.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private readonly IFeedbackRepository _feedbackRepository;
+
+        public FeedbackController(IFeedbackRepository feedbackRepository)
+        {
+            _feedbackRepository = feedbackRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
